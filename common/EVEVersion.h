c137100f2c4d0fd86f838ce8eb26d45c77fd0db8
maysjw <maysjw@53638f16-6364-11df-b3cf-17e690f86674>
2008-12-04 00:00:19 +0000

    * Updating client version to 70393

--- common/EVEVersion.h
+++ common/EVEVersion.h
@@ -5,7 +5,7 @@
 
 static const uint8 MachoNetVersion = 201;
 static const double EVEVersionNumber = 5.20;
-static const uint32 EVEBuildVersion = 70124;
+static const uint32 EVEBuildVersion = 70393;
 static const char *const EVEProjectCodename = "EVE-EVE-RELEASE";
 static const char *const EVEProjectRegion = "ccp";
 static const char *const EVEProjectVersion = "EVE-EVE-RELEASE@ccp";
