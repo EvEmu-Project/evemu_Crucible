9a92bfb6e0142dfc65373b988d9c91e07d3c7d34
bloody.rabbit <bloody.rabbit@53638f16-6364-11df-b3cf-17e690f86674>
2009-03-29 12:19:42 +0000

    * support for Apocrypha build 85476

--- include/common/EVEVersion.h
+++ include/common/EVEVersion.h
@@ -26,11 +26,11 @@
 #ifndef __EVE_VERSION_H
 #define __EVE_VERSION_H
 
-//supported client is the Apocrypha client build 84244
+//supported client is the Apocrypha client build 85476
 
-static const uint8 MachoNetVersion = 205;
+static const uint8 MachoNetVersion = 206;
 static const double EVEVersionNumber = 6.10;
-static const uint32 EVEBuildVersion = 84244;
+static const uint32 EVEBuildVersion = 85476;
 static const char *const EVEProjectCodename = "EVE-EVE-RELEASE";
 static const char *const EVEProjectRegion = "ccp";
 static const char *const EVEProjectVersion = "EVE-EVE-RELEASE@ccp";
