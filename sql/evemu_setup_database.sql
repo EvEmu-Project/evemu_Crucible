73f97ddd0511b979e822acd093f9bf0a565943f0
Aknor Jaden <aknorjaden@gmail.com>
2011-07-10 11:44:39 +0800

    added one-stop-shop sql file to run them all from the /sql/ directory.  you have to have the inc15-mysql5-v1.sql file from http://eve.no-ip.de/inc15/ in the /sql/ directory, and copy liveupdates.sql to /sql/ from the /sql/evemu-updates.  Then run the bats in /sql/utils/.  Now you can run this sql file to populate your DB in one step.  Just be sure to change the name of the DB on the first line of this sql file to match your DB name.

---
+++ sql/evemu_setup_database.sql
@@ -0,0 +1,8 @@
+use evemuincursion;
+source inc15-mysql5-v1.sql;
+source ofic-updates.sql;
+source evemu_static-dump.sql;
+source evemu_dynamic-dump.sql;
+source evemu-updates.sql;
+source prime_db.sql;
+source liveupdates.sql;
