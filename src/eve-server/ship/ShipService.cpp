98b92832b6210ab249771e4649dddf359ea890b0
ozatomic <ozatomic@gmail.com>
2011-08-04 17:03:34 +1000

    Ship Bound was using the wrong packet. Can now enter ship. Login packet was sending the wrong roleid. The database was getting out a uint64 as a uint32. Changed set role to 5003499186008621056 for now as this gets you a big insider bar.

--- src/eve-server/ship/ShipService.cpp
+++ src/eve-server/ship/ShipService.cpp
@@ -95,7 +95,8 @@ PyBoundObject *ShipService::_CreateBoundObject(Client *c, const PyRep *bind_args
 }
 
 PyResult ShipBound::Handle_Board(PyCallArgs &call) {
-    Call_SingleIntegerArg args;
+    //Call_SingleIntegerArg args;
+	Call_TwoIntegerArgs args;
 
     // Save position for old ship
     GPoint shipPosition = call.client->GetPosition();
@@ -109,9 +110,9 @@ PyResult ShipBound::Handle_Board(PyCallArgs &call) {
     }
 	
     // Get ShipRef of the ship we want to board:
-	ShipRef boardShipRef = m_manager->item_factory.GetShip( args.arg );                 // This is too inefficient, use line below
+	ShipRef boardShipRef = m_manager->item_factory.GetShip( args.arg1 );                 // This is too inefficient, use line below
     //ShipRef boardShipRef = call.client->System()->GetShipFromInventory( args.arg );
-    ShipEntity * pShipEntity = (ShipEntity *)(call.client->System()->get( args.arg ));
+    ShipEntity * pShipEntity = (ShipEntity *)(call.client->System()->get( args.arg1 ));
 
     if(call.client->IsInSpace())
     {
@@ -122,7 +123,7 @@ PyResult ShipBound::Handle_Board(PyCallArgs &call) {
 
 		if( !boardShipRef )
         {
-			sLog.Error("ShipBound::Handle_Board()", "%s: Failed to get new ship %u.", call.client->GetName(), args.arg);
+			sLog.Error("ShipBound::Handle_Board()", "%s: Failed to get new ship %u.", call.client->GetName(), args.arg1);
 		}
 		else
 		{
@@ -203,7 +204,7 @@ PyResult ShipBound::Handle_Board(PyCallArgs &call) {
 
     // Player is NOT in space, so board ship as if in station:
     if( !boardShipRef ) {
-        sLog.Error("ShipBound::Handle_Board()", "%s: Failed to get new ship %u.", call.client->GetName(), args.arg);
+        sLog.Error("ShipBound::Handle_Board()", "%s: Failed to get new ship %u.", call.client->GetName(), args.arg1);
     }
     else
 	{
