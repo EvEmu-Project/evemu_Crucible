9edb165a5ee477002b06d7a39e8fb105eb24503b
Stefan Schake <caytchen@gmail.com>
2011-08-24 16:50:48 +0200

    On a second thought, we don't want incremental updates
    Signed-off-by: Stefan Schake <caytchen@gmail.com>

--- sql/incremental/respec-support.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `character_`
-	ADD COLUMN `freeRespecs` INT(10) UNSIGNED NULL DEFAULT '0' AFTER `online`,
-	ADD COLUMN `nextRespec` BIGINT(20) UNSIGNED NULL DEFAULT '0' AFTER `freeRespecs`;
