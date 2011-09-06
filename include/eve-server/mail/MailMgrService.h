caad2fdedb59c0be692eff286efc2871fbf2523a
Stefan Schake <caytchen@gmail.com>
2011-09-06 22:00:11 +0200

    Update license; add it to source, too
    Signed-off-by: Stefan Schake <caytchen@gmail.com>

--- include/eve-server/mail/MailMgrService.h
+++ include/eve-server/mail/MailMgrService.h
@@ -20,7 +20,7 @@
 	Place - Suite 330, Boston, MA 02111-1307, USA, or go to
 	http://www.gnu.org/copyleft/lesser.txt.
 	------------------------------------------------------------------------------------
-	Author:		Luck
+	Author:		Luck, caytchen
 */
 
 #ifndef MAIL_MGR_SERIVCE_H
