638385482efea28d88a8725af01ff3a781b1450d
Stefan Schake <caytchen@gmail.com>
2011-09-07 20:50:48 +0200

    Add MailDB dummy
    Signed-off-by: Stefan Schake <caytchen@gmail.com>

--- include/eve-server/EVEServerPCH.h
+++ include/eve-server/EVEServerPCH.h
@@ -226,10 +226,11 @@
 #include "inventory/ItemType.h"
 #include "inventory/Owner.h"
 
-//mail stuff
+// mail stuff
 #include "mail/MailMgrService.h"
 #include "mail/MailingListMgrService.h"
 #include "mail/NotificationMgrService.h"
+#include "mail/MailDB.h"
 
 // factory stuff
 #include "manufacturing/Blueprint.h"
