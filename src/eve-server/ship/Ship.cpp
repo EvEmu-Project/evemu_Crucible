faf42084c4df270ff50a1c198cb3ee36592eaca1
ozatomic <ozatomic@levelbelow.net>
2012-03-02 15:23:23 +1100

    Save ship attributes on Spawn

--- src/eve-server/ship/Ship.cpp
+++ src/eve-server/ship/Ship.cpp
@@ -142,6 +142,9 @@ ShipRef Ship::Spawn(ItemFactory &factory,
     if( !(sShipRef.get()->HasAttribute(AttrWarpSpeedMultiplier)) )
         sShipRef.get()->SetAttribute(AttrWarpSpeedMultiplier, 1.0f);
 
+	//Save The Ships Attributes.
+	sShipRef->SaveAttributes();
+
     return sShipRef;
 }
 
