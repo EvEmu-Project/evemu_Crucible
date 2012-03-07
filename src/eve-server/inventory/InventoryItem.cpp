6b776e4b0e96f0768066664bdd53671f41595441
ozatomic <ozatomic@levelbelow.net>
2012-03-08 10:44:05 +1100

    Singltons are now passed to the client as -1 instead of singlton Y qty 1

--- src/eve-server/inventory/InventoryItem.cpp
+++ src/eve-server/inventory/InventoryItem.cpp
@@ -636,7 +636,13 @@ void InventoryItem::GetItemRow( PyPackedRow* into ) const
     into->SetField( "ownerID",    new PyInt( ownerID() ) );
 	into->SetField( "locationID", new PyLong( locationID() ) );
     into->SetField( "flagID",     new PyInt( flag() ) );
-    into->SetField( "quantity",   new PyInt( quantity() ) );
+	if(singleton())
+	{
+		into->SetField( "quantity",   new PyInt( -1 ) );
+	} else
+	{
+        into->SetField( "quantity",   new PyInt( quantity() ) );
+	}
     into->SetField( "groupID",    new PyInt( groupID() ) );
     into->SetField( "categoryID", new PyInt( categoryID() ) );
     into->SetField( "customInfo", new PyString( customInfo() ) );
