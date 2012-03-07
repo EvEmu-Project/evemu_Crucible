eb2fa1ee053879472c767ca94ebbfd3ae5032fb9
ozatomic <ozatomic@levelbelow.net>
2012-03-08 10:54:13 +1100

    caytchen told me to save some lines since we not getting paid for LOC

--- src/eve-server/inventory/InventoryItem.cpp
+++ src/eve-server/inventory/InventoryItem.cpp
@@ -636,13 +636,7 @@ void InventoryItem::GetItemRow( PyPackedRow* into ) const
     into->SetField( "ownerID",    new PyInt( ownerID() ) );
 	into->SetField( "locationID", new PyLong( locationID() ) );
     into->SetField( "flagID",     new PyInt( flag() ) );
-	if(singleton())
-	{
-		into->SetField( "quantity",   new PyInt( -1 ) );
-	} else
-	{
-        into->SetField( "quantity",   new PyInt( quantity() ) );
-	}
+	into->SetField( "quantity",   new PyInt( singleton() ? -1 : quantity()) );
     into->SetField( "groupID",    new PyInt( groupID() ) );
     into->SetField( "categoryID", new PyInt( categoryID() ) );
     into->SetField( "customInfo", new PyString( customInfo() ) );
