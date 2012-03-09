cdb9ca4fd46ff7a0e6819cc725b8497cedede7c0
ReveMu <gheorghitacristian@mac.com>
2012-03-10 01:41:10 +0200

    fixed GetFormations error

--- src/eve-server/ship/BeyonceService.cpp
+++ src/eve-server/ship/BeyonceService.cpp
@@ -123,7 +123,7 @@ PyResult BeyonceService::Handle_GetFormations(PyCallArgs &call) {
 	//now we know its in the cache one way or the other, so build a 
 	//cached object cached method call result.
 	//return(m_manager->cache_service->MakeObjectCachedMethodCallResult(method_id));
-	return NULL;
+	return new PyTuple(0);
 }
 
 /*
