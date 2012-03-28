3101630361da93443099529bf14403862d4a7ea7
Stefan Schake <caytchen@gmail.com>
2012-03-28 06:47:18 +0200

    Implement MailingListMgrService stub
    Signed-off-by: Stefan Schake <caytchen@gmail.com>

--- include/eve-server/mail/MailingListMgrService.h
+++ include/eve-server/mail/MailingListMgrService.h
@@ -1,4 +1,3 @@
-
 #ifndef MAILING_LIST_MGR_SERVICE_H
 #define MAILING_LIST_MGR_SERVICE_H
 
@@ -14,7 +13,24 @@ protected:
 	class Dispatcher;
 	Dispatcher *const m_dispatch;
 
-	//add PyCallable_DECL_CALL(function_name) here
+	PyCallable_DECL_CALL(GetJoinedLists)
+	PyCallable_DECL_CALL(Create)
+	PyCallable_DECL_CALL(Join)
+	PyCallable_DECL_CALL(Leave)
+	PyCallable_DECL_CALL(Delete)
+	PyCallable_DECL_CALL(KickMembers)
+	PyCallable_DECL_CALL(GetMembers)
+	PyCallable_DECL_CALL(SetEntityAccess)
+	PyCallable_DECL_CALL(ClearEntityAccess)
+	PyCallable_DECL_CALL(SetMembersMuted)
+	PyCallable_DECL_CALL(SetMembersOperator)
+	PyCallable_DECL_CALL(SetMembersClear)
+	PyCallable_DECL_CALL(SetDefaultAccess)
+	PyCallable_DECL_CALL(GetSettings)
+	PyCallable_DECL_CALL(GetWelcomeMail)
+	PyCallable_DECL_CALL(SaveWelcomeMail)
+	PyCallable_DECL_CALL(SendWelcomeMail)
+	PyCallable_DECL_CALL(ClearWelcomeMail)
 };
 
 #endif /* MAILING_LIST_MGR_SERVICE_H */
