1c28abe6b6ff39d984bdc11802b6bf2546842662
ozatomic <evemu@ks213311.kimsufi.com>
2012-04-17 08:47:03 +1000

    Updated to latest client (360229)

--- include/eve-common/EVEVersion.h
+++ include/eve-common/EVEVersion.h
@@ -29,8 +29,8 @@
 // supported client is Crucible v1.0.1 build 336683
 
 static const uint16 MachoNetVersion = 320;
-static const double EVEVersionNumber = 7.30;
-static const int32 EVEBuildVersion = 353549;
+static const double EVEVersionNumber = 7.31;
+static const int32 EVEBuildVersion = 360229;
 static const char *const EVEProjectCodename = "EVE-EVE-TRANQUILITY";
 static const char *const EVEProjectRegion = "ccp";
 static const char *const EVEProjectVersion = "EVE-EVE-TRANQUILITY@ccp";
