5c6300772bade8a7c5120a66582322c4ee6c41bc
Bloody.Rabbit <jan.bobek@gmail.com>
2012-04-24 20:58:18 +0200

    cmake: Improve generated version string, add default version.

--- cmake/GitTreeInfo.cmake
+++ cmake/GitTreeInfo.cmake
@@ -8,10 +8,11 @@
 #   <prefix> - prefix for output variables
 #
 # Following variables are defined:
-#   <prefix>_BRANCH  - name of current branch (may be empty)
-#   <prefix>_HASH    - short commit hash
-#   <prefix>_TAG     - current tag (may be empty)
-#   <prefix>_VERSION - a nice version string
+#   <prefix>_BRANCH    - name of current branch
+#   <prefix>_HASH      - short commit hash
+#   <prefix>_TAG_EXACT - tag of exact current commit
+#   <prefix>_TAG_LAST  - last tag relative to current commit
+#   <prefix>_VERSION   - a nice version string
 #
 
 MACRO( GIT_TREE_INFO PATH PREFIX )
@@ -30,19 +31,37 @@ MACRO( GIT_TREE_INFO PATH PREFIX )
                    OUTPUT_STRIP_TRAILING_WHITESPACE
                    ERROR_QUIET )
 
-  # Obtain tag
+  # Obtain exact tag
   EXECUTE_PROCESS( COMMAND "${GIT_EXECUTABLE}" describe --tags --exact-match
                    WORKING_DIRECTORY "${PATH}"
-                   OUTPUT_VARIABLE "${PREFIX}_TAG"
+                   OUTPUT_VARIABLE "${PREFIX}_TAG_EXACT"
+                   OUTPUT_STRIP_TRAILING_WHITESPACE
+                   ERROR_QUIET )
+
+  # Obtain last tag
+  EXECUTE_PROCESS( COMMAND "${GIT_EXECUTABLE}" describe --tags --abbrev=0
+                   WORKING_DIRECTORY "${PATH}"
+                   OUTPUT_VARIABLE "${PREFIX}_TAG_LAST"
                    OUTPUT_STRIP_TRAILING_WHITESPACE
                    ERROR_QUIET )
 
   # Build a nice version string
-  IF( "${PREFIX}_TAG" )
-    SET( "${PREFIX}_VERSION" "${${PREFIX}_TAG}" )
-  ELSEIF( NOT "${PREFIX}_BRANCH" )
-    SET( "${PREFIX}_VERSION" "${${PREFIX}_HASH}" )
-  ELSE( "${PREFIX}_TAG" )
+  IF( "${PREFIX}_TAG_EXACT" )
+    # We have an exact tag, use that and nothing else
+    SET( "${PREFIX}_VERSION" "${${PREFIX}_TAG_EXACT}" )
+  ELSEIF( "${PREFIX}_TAG_LAST" )
+    IF( "${PREFIX}_BRANCH" )
+      # We have the last tag and a branch, use tag + branch + hash
+      SET( "${PREFIX}_VERSION" "${${PREFIX}_TAG_LAST}-${${PREFIX}_BRANCH}-${${PREFIX}_HASH}" )
+    ELSE( "${PREFIX}_BRANCH" )
+      # We have the last tag but no branch, use tag + hash
+      SET( "${PREFIX}_VERSION" "${${PREFIX}_TAG_LAST}-${${PREFIX}_HASH}" )
+    ENDIF( "${PREFIX}_BRANCH" )
+  ELSEIF( "${PREFIX}_BRANCH" )
+    # No last tag but we have a branch, use branch + hash
     SET( "${PREFIX}_VERSION" "${${PREFIX}_BRANCH}-${${PREFIX}_HASH}" )
-  ENDIF( "${PREFIX}_TAG" )
+  ELSE( "${PREFIX}_TAG_EXACT" )
+    # No last tag and no branch, use only hash
+    SET( "${PREFIX}_VERSION" "${${PREFIX}_HASH}" )
+  ENDIF( "${PREFIX}_TAG_EXACT" )
 ENDMACRO( GIT_TREE_INFO )
