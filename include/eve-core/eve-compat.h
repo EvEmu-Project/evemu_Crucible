44a188d7a07cb59b48b5fcae591e8fc02bd185d2
Bloody.Rabbit <jan.bobek@gmail.com>
2012-06-25 14:24:35 +0200

    cmake: Add more finite, isnan checks and change the order of their preference.

--- include/eve-core/eve-compat.h
+++ include/eve-core/eve-compat.h
@@ -125,23 +125,29 @@ typedef unsigned __int64 uint64;
 #   define asinh boost::math::asinh
 #endif /* !HAVE_ASINH */
 
-#if defined( HAVE__FINITE )
-#   define finite _finite
-#elif defined( HAVE___FINITE )
-#   define finite __finite
-#elif defined( HAVE_STD_FINITE )
-#   define finite std::finite
-#elif defined( HAVE_STD_ISFINITE )
-#   define finite std::isfinite
-#endif
+#ifndef HAVE_FINITE
+#   if defined( HAVE_ISFINITE )
+#       define finite isfinite
+#   elif defined( HAVE_STD_FINITE )
+#       define finite std::finite
+#   elif defined( HAVE_STD_ISFINITE )
+#       define finite std::isfinite
+#   elif defined( HAVE__FINITE )
+#       define finite _finite
+#   elif defined( HAVE___FINITE )
+#       define finite __finite
+#   endif /* HAVE___FINITE */
+#endif /* !HAVE_FINITE */
 
-#if defined( HAVE__ISNAN )
-#   define isnan _isnan
-#elif defined( HAVE___ISNAN )
-#   define isnan __isnan
-#elif defined( HAVE_STD_ISNAN )
-#   define isnan std::isnan
-#endif
+#ifndef HAVE_ISNAN
+#   if defined( HAVE_STD_ISNAN )
+#       define isnan std::isnan
+#   elif defined( HAVE__ISNAN )
+#       define isnan _isnan
+#   elif defined( HAVE___ISNAN )
+#       define isnan __isnan
+#   endif /* HAVE___ISNAN */
+#endif /* !HAVE_ISNAN */
 
 /*************************************************************************/
 /* cstdarg                                                               */
