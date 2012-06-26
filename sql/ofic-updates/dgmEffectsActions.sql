ca44edea0b4fe5a77648dfdef90fee608f1d1e0a
Aknor Jaden <aknorjaden@gmail.com>
2012-06-26 00:28:45 -0400

    Forgot to save changes to sql file for dgmEffectsActions table.

--- sql/ofic-updates/dgmEffectsActions.sql
+++ sql/ofic-updates/dgmEffectsActions.sql
@@ -19,6 +19,8 @@ CREATE TABLE IF NOT EXISTS `dgmEffectsActions` (
   `effectAppliedInState` int(11) NOT NULL,
   `effectAppliedTo` int(11) NOT NULL,
   `effectApplicationType` int(11) NOT NULL,
+  `targetEquipmentType` int(11) NOT NULL,
+  `targetGroupIDs` text NOT NULL,
   `stackingPenaltyApplied` int(11) NOT NULL,
   `nullifyOnlineEffect` int(11) NOT NULL,
   `nullifiedOnlineEffectID` int(11) NOT NULL,
