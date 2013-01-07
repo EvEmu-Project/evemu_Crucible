5bff75e240f726d805988d3062504409502c5593
Aknor Jaden <aknorjaden@gmail.com>
2013-01-06 21:56:40 -0500

    Fixed attributes not being saved into the Database table entity_attributes by making sure mChanged bool is set to true when AttributeMap::SetAttribute() is called to change an attribute in the map for an InventoryItem.  Also started some work, though not finished, to address the killing of a ship controlled by a Client object.  Code located in Damage.cpp Client::Killed()

--- sql/evemu_setup_database.sql
+++ sql/evemu_setup_database.sql
@@ -1,7 +1,7 @@
 use evemu;
 source cru16-mysql5-v1.sql;
-source ofic-updates.sql;
-source evemu_static-dump.sql;
 source evemu_dynamic-dump.sql;
+source evemu_static-dump.sql;
+source ofic-updates.sql;
 source prime_db.sql;
 source liveupdates.sql;
