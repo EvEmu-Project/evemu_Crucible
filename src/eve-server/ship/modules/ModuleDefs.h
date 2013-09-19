8097e50b0ff81c6fd64c0628ee4ca26f85cc8e5f
Aknor Jaden <aknorjaden@gmail.com>
2013-09-19 01:26:22 -0400

    Modified Module State and Calculation enums to reflect the EVEHQ data import effort.  Updated all calculation formula methods and the switch statement routing execution to one of these based on the calculation type ID.  These changes now are 100% compatible with the EVEHQ data import.

--- src/eve-server/ship/modules/ModuleDefs.h
+++ src/eve-server/ship/modules/ModuleDefs.h
@@ -50,12 +50,13 @@ enum ModuleCommand
 // *** use these values to decode the 'effectAppliedInState' field of the 'dgmEffectsActions' database table
 enum ModuleStates
 {
-    MOD_UNFITTED = 1100,
-    MOD_OFFLINE,
-    MOD_ONLINE,
-    MOD_ACTIVATED,
-    MOD_OVERLOADED,
-    MOD_DEACTIVATING
+    MOD_UNFITTED = 0,
+    MOD_OFFLINE = 1,
+    MOD_ONLINE = 2,
+    MOD_ACTIVATED = 4,
+    MOD_OVERLOADED = 8,
+    MOD_GANG = 16,
+	MOD_FLEET = 32
 };
 
 enum ChargeStates
@@ -140,49 +141,111 @@ enum ModulePowerLevel
 // *** use these values to decode the 'calculationTypeID' and the 'reverseCalculationTypeID' fields of the 'dgmEffectsInfo' database table
 enum EVECalculationType
 {
-    CALC_NONE = 2000,
-    CALC_AUTO,
-    CALC_ADD,
-    CALC_SUBTRACT,
-    CALC_DIVIDE,
-    CALC_MULTIPLY,
-    CALC_ADD_PERCENT,
-    CALC_REV_ADD_PERCENT,
-    CALC_SUBTRACT_PERCENT,
-    CALC_REV_SUBTRACT_PERCENT,
-    CALC_ADD_AS_PERCENT,
-    CALC_SUBTRACT_AS_PERCENT,
-    CALC_MODIFY_PERCENT_W_PERCENT,
-    CALC_REV_MODIFY_PERCENT_W_PERCENT,
-    CALC_MWD_AB_EQUATION,
-	CALC_ECM_JAM_EQUATION,
-	CALC_REDUCE_BY_PERCENT,
-	CALC_REV_REDUCE_BY_PERCENT
+    CALC_NONE = -1,
+    CALC_PERCENTAGE = 0,
+    CALC_ADDITION = 1,
+    CALC_DIFFERENCE = 2,
+	CALC_VELOCITY = 3,
+    CALC_ABSOLUTE = 4,
+	CALC_MULTIPLIER = 5,
+    CALC_ADD_POSITIVE = 6,
+    CALC_ADD_NEGATIVE = 7,
+    CALC_SUBTRACTION = 8,
+    CALC_CLOAKED_VELOCITY = 9,
+    CALC_SKILL_LEVEL = 10,
+    CALC_SKILL_LEVEL_x_ATT = 11,
+    CALC_ABSOLUTE_MAX = 12,
+    CALC_ABSOLUTE_MIN = 13,
+    CALC_CAP_BOOSTERS = 14
     //more will show up, im sure
 };
 
 
-//TODO - check mem usage
-static EvilNumber Add(EvilNumber &val1, EvilNumber &val2)
+static EvilNumber Percentage(EvilNumber &attributeToModify, EvilNumber &modifierValue)
 {
-    return val1 + val2;
+	return (attributeToModify * (EvilNumber(1.0) + (modifierValue / EvilNumber(100.0))));
 }
 
-static EvilNumber Subtract(EvilNumber &val1, EvilNumber &val2)
+static EvilNumber Addition(EvilNumber &attributeToModify, EvilNumber &modifierValue)
 {
-    return val1 - val2;
+    return (attributeToModify + modifierValue);
 }
 
-static EvilNumber Divide(EvilNumber &val1, EvilNumber &val2)
+static EvilNumber Difference(EvilNumber &attributeToModify, EvilNumber &modifierValue)
 {
-    return ( val1 / val2 );
+	if( modifierValue <= 0 )
+		return (((EvilNumber(100.0) - attributeToModify) * (-modifierValue / EvilNumber(100))) + attributeToModify);
+	else
+		return ((attributeToModify * (-modifierValue / EvilNumber(100.0))) + attributeToModify);
 }
 
-static EvilNumber Multiply(EvilNumber &val1, EvilNumber &val2)
+static EvilNumber Velocity(EvilNumber &attributeToModify, EvilNumber &modifierValue)
 {
-    return val1 * val2;
+	// In this special case, it is expected that modifierValue is actually the thrust/mass ratio multiplied by the module effect source attribute:
+	return (attributeToModify + (attributeToModify * modifierValue / EvilNumber(100.0)));
 }
 
+//static EvilNumber Divide(EvilNumber &val1, EvilNumber &val2)
+//{
+//    return ( val1 / val2 );
+//}
+
+static EvilNumber Multiplier(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+    return (attributeToModify * modifierValue);
+}
+
+static EvilNumber AddPositive(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+	if( modifierValue > 0 )
+		return (attributeToModify + modifierValue);
+	else
+		return (attributeToModify);
+}
+
+static EvilNumber AddNegative(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+	if( modifierValue < 0 )
+		return (attributeToModify + modifierValue);
+	else
+		return (attributeToModify);
+}
+
+static EvilNumber Subtraction(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+    return (attributeToModify - modifierValue);
+}
+
+static EvilNumber CloakedVelocity(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+	return (EvilNumber(-100.0) + ((EvilNumber(100.0) + attributeToModify * (modifierValue / EvilNumber(100.0)))));
+}
+
+static EvilNumber AbsoluteMax(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+	if( attributeToModify > modifierValue )
+		return attributeToModify;
+	else
+		return modifierValue;
+}
+
+static EvilNumber AbsoluteMin(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+	if( attributeToModify < modifierValue )
+		return attributeToModify;
+	else
+		return modifierValue;
+}
+
+static EvilNumber CapBoosters(EvilNumber &attributeToModify, EvilNumber &modifierValue)
+{
+	if( (attributeToModify - modifierValue) < 0 )
+		return (attributeToModify - modifierValue);
+	else
+		return EvilNumber(0.0);
+}
+
+/*
 static EvilNumber AddPercent(EvilNumber &val1, EvilNumber &val2)
 {
     return val1 + ( val1 * val2 );
@@ -250,27 +313,44 @@ static EvilNumber ReverseReduceByPercent(EvilNumber &val1, EvilNumber &val2)
 
 	return val1 / ( val3 - (val2 / val4) );
 }
+*/
 
 static EvilNumber CalculateNewAttributeValue(EvilNumber attrVal, EvilNumber attrMod, EVECalculationType type)
 {
     switch(type)
     {
         case CALC_NONE :                            return attrVal;
-        case CALC_AUTO :                            return attrVal;                             // AUTO NOT SUPPORTED AT THIS TIME !!!
-        case CALC_ADD :                             return Add(attrVal, attrMod);
-        case CALC_SUBTRACT :                        return Subtract(attrVal, attrMod);
-        case CALC_DIVIDE :                          return Divide(attrVal, attrMod);
-        case CALC_MULTIPLY :                        return Multiply(attrVal, attrMod);
-        case CALC_ADD_PERCENT :                     return AddPercent(attrVal, attrMod);
-        case CALC_REV_ADD_PERCENT :                 return ReverseAddPercent(attrVal, attrMod);
-        case CALC_SUBTRACT_PERCENT :                return SubtractPercent(attrVal, attrMod);
-        case CALC_REV_SUBTRACT_PERCENT :            return ReverseSubtractPercent(attrVal, attrMod);
-        case CALC_ADD_AS_PERCENT :                  return AddAsPercent(attrVal, attrMod);
-        case CALC_SUBTRACT_AS_PERCENT :             return SubtractAsPercent(attrVal, attrMod);
-        case CALC_MODIFY_PERCENT_W_PERCENT :        return ModifyPercentWithPercent(attrVal, attrMod);
-        case CALC_REV_MODIFY_PERCENT_W_PERCENT :    return ReverseModifyPercentWithPercent(attrVal, attrMod);
-		case CALC_REDUCE_BY_PERCENT:				return ReduceByPercent(attrVal, attrMod);
-		case CALC_REV_REDUCE_BY_PERCENT :			return ReverseReduceByPercent(attrVal, attrMod);
+		case CALC_PERCENTAGE :						return Percentage(attrVal, attrMod); break;
+		case CALC_ADDITION :						return Addition(attrVal, attrMod); break;
+		case CALC_DIFFERENCE :						return Difference(attrVal, attrMod); break;
+		case CALC_VELOCITY :						return Velocity(attrVal, attrMod); break;
+		case CALC_ABSOLUTE :						return attrVal; break;
+		case CALC_MULTIPLIER :						return Multiplier(attrVal, attrMod); break;
+		case CALC_ADD_POSITIVE :					return AddPositive(attrVal, attrMod); break;
+		case CALC_ADD_NEGATIVE :					return AddNegative(attrVal, attrMod); break;
+		case CALC_SUBTRACTION :						return Subtraction(attrVal, attrMod); break;
+		case CALC_CLOAKED_VELOCITY :				return CloakedVelocity(attrVal, attrMod); break;
+		case CALC_SKILL_LEVEL :						return attrVal; break;	// is this really right for attribute effect per skill level?
+		case CALC_SKILL_LEVEL_x_ATT :				return attrVal; break;	// is this really right for attribute effect per skill level?
+		case CALC_ABSOLUTE_MAX :					return AbsoluteMax(attrVal, attrMod); break;
+		case CALC_ABSOLUTE_MIN :					return AbsoluteMin(attrVal, attrMod); break;
+		case CALC_CAP_BOOSTERS :					return CapBoosters(attrVal, attrMod); break;
+        //case CALC_AUTO :                            return attrVal; break;                             // AUTO NOT SUPPORTED AT THIS TIME !!!
+        //case CALC_ADD :                             return Add(attrVal, attrMod); break;
+        //case CALC_SUBTRACT :                        return Subtract(attrVal, attrMod); break;
+        //case CALC_DIVIDE :                          return Divide(attrVal, attrMod); break;
+        //case CALC_MULTIPLY :                        return Multiply(attrVal, attrMod); break;
+        //case CALC_ADD_PERCENT :                     return AddPercent(attrVal, attrMod); break;
+        //case CALC_REV_ADD_PERCENT :                 return ReverseAddPercent(attrVal, attrMod); break;
+        //case CALC_SUBTRACT_PERCENT :                return SubtractPercent(attrVal, attrMod); break;
+        //case CALC_REV_SUBTRACT_PERCENT :            return ReverseSubtractPercent(attrVal, attrMod); break;
+        //case CALC_ADD_AS_PERCENT :                  return AddAsPercent(attrVal, attrMod); break;
+        //case CALC_SUBTRACT_AS_PERCENT :             return SubtractAsPercent(attrVal, attrMod); break;
+        //case CALC_MODIFY_PERCENT_W_PERCENT :        return ModifyPercentWithPercent(attrVal, attrMod); break;
+        //case CALC_REV_MODIFY_PERCENT_W_PERCENT :    return ReverseModifyPercentWithPercent(attrVal, attrMod); break;
+		//case CALC_REDUCE_BY_PERCENT:				return ReduceByPercent(attrVal, attrMod); break;
+		//case CALC_REV_REDUCE_BY_PERCENT :			return ReverseReduceByPercent(attrVal, attrMod); break;
+		default:									return 0; break;
     }
 
     sLog.Error("CalculateNewAttributeValue", "Unknown EveCalculationType used");
