f98e9d10c234294c52d7dd47174fbd553189293b
Aknor Jaden <aknorjaden@gmail.com>
2013-09-19 01:17:28 -0400

    Fixed some syntax errors, expanded the verbose option printing, changed mysql server connection to use arguments from command line, and corrected checking for no data returned from eve-central.  While a full test run has not been performed, a small number of items were tested and all prices seem to be as expected so far.

--- sql/utils/eve-central_to_database.py
+++ sql/utils/eve-central_to_database.py
@@ -151,29 +151,38 @@ if (option_valid == 0):
     sys.exit(0)
 
 # Unit Test command line argument extraction:
-#print mysql_server_IP
-#print mysql_user
-#print mysql_password
-#print mysql_db_name
-#print option
+print ""
+print "Arguments you entered:"
+print "<mysql_server_IP> = ", mysql_server_IP
+print "<mysql_user> = ", mysql_user
+print "<mysql_password> = ", mysql_password
+print "<mysql_db_name> = ", mysql_db_name
+print "option = ", option
+print ""
 #sys.exit(0)
     
 # MySQLDB: Connect to the MySQL Database
 db = MySQLdb.connect(
-        host = "127.0.0.1",
-        user = "root",
-        passwd = "password",
-        db = "evemu" )
+        host = mysql_server_IP,
+        user = mysql_user,
+        passwd = mysql_password,
+        db = mysql_db_name )
+
+print "CONNECTED TO DATABASE '", mysql_db_name, "' as user '", mysql_user, "'"
+print ""
 
 # MySQLDB: prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-# MySQLDB: Query mapRegions database table to get list of regionIDs 
+# MySQLDB: Query mapRegions database table to get list of regionIDs
+print "Getting mapRegions..."
 cursor.execute("\
  SELECT\
    `regionID`, `regionName`\
  FROM `mapRegions`\
  WHERE 1")
+print "DONE!"
+print ""
 
 results = cursor.fetchall()
 
@@ -198,7 +207,8 @@ else:
     regionID_List_length = len(regionID_List)
 
 
-# MySQLDB: Query invTypes database table to get list of typeIDs 
+# MySQLDB: Query invTypes database table to get list of typeIDs
+print "Getting invTypes..."
 cursor.execute("\
  SELECT\
    `typeID`, `typeName`, `basePrice`\
@@ -207,6 +217,8 @@ cursor.execute("\
    `groupID` IN\
      (SELECT `groupID` FROM `invGroups` WHERE `categoryID` IN\
        (4,5,6,7,8,9,16,17,18,20,22,23,24,25,32,34,35,39,40,41,42,43,46))")
+print "DONE!"
+print ""
 
 results = cursor.fetchall()
 
@@ -228,21 +240,31 @@ typeID_List_length = len(typeID_List)
 
 
 # Outer Loop - Loop through all regionIDs
+print "PROCESSING REQUEST..."
 for region in range(0,regionID_List_length):
     regionID = regionID_List[region]
     regionProgress = 100.0 * ((1.0*region)/regionID_List_length)
 
     # Inner Loop - Loop through entire list of typeIDs
-    for index in range(0,10): #range(1,typeID_List_length-1)
+    for index in range(0,30): #range(1,typeID_List_length-1)
         typeID = typeID_List[index]
         typeID_Progress = 100.0 * ((1.0*index)/typeID_List_length)
 
+        if (verbose == 1):
+            print "Getting eve-central data for typeID ", typeID_List[index], " - ", typeName_List[index]
         contents = eve_central_query_market_data(regionID,option)
+        if (verbose == 1):
+            print "Response from eve-central:"
+            print contents
+            print "END RESPONSE"
         
-        if (contents != "The request contains bad syntax or cannot be fulfilled."):
+        if (contents[0:5] == "<?xml"):
             # Parse the XML response from EVE-Central
             # See usage here for ElementTree:  http://effbot.org/zone/element-index.htm
             tree = XML(contents)        # From a string
+            if (verbose == 1):
+                print "XML received from eve-central:"
+                print tree
             #print "XML Element count = ", len(tree)
 
             # the tree root is the toplevel html element
@@ -250,17 +272,17 @@ for region in range(0,regionID_List_length):
             root = tree
 
             for c in root.getchildren():
-                #if (verbose == 1):
-                    #print c.tag
+                if (verbose == 1):
+                    print c.tag
                 for d in c.getchildren():
-                    #if (verbose == 1):
-                        #print "    ", d.tag
+                    if (verbose == 1):
+                        print "    ", d.tag
                     for e in d.getchildren():
-                        #if (verbose == 1):
-                            #print "        ", e.tag
+                        if (verbose == 1):
+                            print "        ", e.tag
                         for f in e.getchildren():
-                            #if (verbose == 1):
-                                #print "            ", f.tag, " = ", f.text
+                            if (verbose == 1):
+                                print "            ", f.tag, " = ", f.text
                             if ((e.tag == "sell") and (f.tag == "avg")):
                                 avg_sell_price = float(f.text)
                             if ((e.tag == "buy") and (f.tag == "avg")):
@@ -271,7 +293,7 @@ for region in range(0,regionID_List_length):
                                 if (option == options[0]):
                                     #create mysql query to update table data
                                     mysql_update_invTypes_basePrice(db, cursor, typeID, avg_sell_price, verbose)
-                                    print "[%.2f" % typeID_Progress, "%] ", "typeID", int(typeID), "[", typeName_List[index], "]  BASE PRICE UPDATED !"
+                                    print "[%.2f" % typeID_Progress, "%] ", "typeID", int(typeID), "[", typeName_List[index], "]  BASE PRICE UPDATED: ", avg_sell_price, "ISK"
                                 
                                 if (option == options[1]):
                                     print "OPTION '", options[1], "' not supported at this time!"
@@ -295,6 +317,7 @@ for region in range(0,regionID_List_length):
         else:
             print "[%.2f" % typeID_Progress, "%] ", "typeID", int(typeID), "[", typeName_List[index], "]  NO DATA... SKIPPING..."
 
+print "DONE!"
 
 # if you need the root element, use getroot
 #root = tree.getroot()
@@ -304,6 +327,7 @@ for region in range(0,regionID_List_length):
 #tree.write("out.xml")
 
 # MYSQLDB: Close the connection to the database so we can exit
+print "CLOSING DATABSE CONNECTION"
 db.close()
 
 sys.exit(0)
