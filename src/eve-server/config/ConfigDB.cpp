fec2d15b3642b74854dc77b2b1bb9ac1215dcc3f
Allan <zhyrohaad@bellsouth.net>
2014-07-10 16:17:47 -0500

    revert GetDynamicCelestials code from very old update that didnt work.

--- src/eve-server/config/ConfigDB.cpp
+++ src/eve-server/config/ConfigDB.cpp
@@ -464,19 +464,19 @@ PyRep *ConfigDB::GetDynamicCelestials(uint32 solarSystemID) {
     sLog.Error("ConfigDB::GetDynamicCelestials", "This query is intentionally made to yield an empty result.  It must be re-worked to provide actual data!");
 
     const std::string query = " SELECT "
-                              "     e.itemID, "
-                              "     e.typeID, "
-                              "     t.groupID, "
-                              "     e.itemName, "
-                              "     m.orbitID, "
-                              "     0, " // This field refers to the boolean value of isConnector.  It may signify some sort of mission jump bridge  -there is no information in DB on this, that I could find.
-                              "     e.x, "
-                              "     e.y, "
-                              "     e.z "
-                              " FROM entity AS e"
-                              "  LEFT JOIN invTypes AS t USING (typeID)"
-                              "  LEFT JOIN mapDenormalize AS m USING (itemID)"
-                              " WHERE e.locationID = %u";
+                              " `itemID`, "
+                              " `entity`.`typeID`, "
+                              " `invTypes`.`groupID`, "
+                              " `itemName`, "
+                              " 0, " // This field refers to the orbitID of the dynamic celestial and needs to be implemented
+                              " 0, " // This field refers to the boolean value of isConnector. It may signify some sort of mission jump bridge
+                              " `x`, "
+                              " `y`, "
+                              " `z` "
+                              " FROM `entity` JOIN `invTypes` ON `entity`.`typeID` = `invTypes`.`typeID`"
+                              " WHERE "
+                              " `locationID` = %u AND " // In the future, the locationID field needs to be constrained to being a solarSystemID
+                              " `groupID` = -1"; // This is set to -1 because we do not know what the ID(s) of dynamic celestials is/are.
 
     DBQueryResult result;
     DBResultRow currentRow;
