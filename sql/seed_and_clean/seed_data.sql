d1bd2bc748192934d806848839143e86cf38d76c
root <root@dev.ke5jro.net>
2021-04-01 19:08:31 -0500

    cleanup old sql fix and update market seeding data
    Signed-off-by: root <root@dev.ke5jro.net>

---
+++ sql/seed_and_clean/seed_data.sql
@@ -0,0 +1,63 @@
+
+
+set @stationid=60014809;     --Ryddinjorn VIM2 - Pator Tech School
+set @solarSystemID=30003410; --Ryddinjorn  - minmatar noob system for pator tech (pts)
+set @regionid=10000042;      --metropolis
+-----------------
+set @stationid=60014140;     --Zemalu IXM2 - Thukker Mix Factory(60014140)
+set @solarSystemID=30000055; --Zemalu
+set @regionid=10000001;      --Derelik
+-----------------
+set @stationid=60014137;     --Ibaria III - Thukker Mix Warehouse(60014137)
+set @solarSystemID=30000053; --Ibaria
+set @regionid=10000001;      --Derelik
+-----------------
+set @stationid=60004591;     --Abudban IX - Brutor Tribe Bureau
+set @solarSystemID=30002507; --Abudban
+set @regionid=10000030;      --Heimatar
+-----------------
+set @stationid=60014779;     --Hulm VIII - Republic University
+set @solarSystemID=30002505; --Hulm     - minmatar noob system for republic uni. (run)
+set @regionid=10000030;      --Heimatar
+-----------------
+set @stationid=60012178;     --Aranir VIIIM8 - Ammatar Fleet Logistic Support
+set @solarSystemID=30000092; --Aranir
+set @regionid=10000001;      --Derelik
+-----------------
+set @stationid=60009112;     --Odatrik V - TransStellar Shipping Storage
+set @solarSystemID=30002509; --Odatrik
+set @regionid=10000030;      --Heimatar
+-----------------
+set @stationid=60012949;     --Juddi VII - DED Logistic Support
+set @solarSystemID=30000051; --Mossas
+set @regionid=10000001;      --Derelik
+
+
+-- regionID, 02: The Forge - 01:derelik - 30:heimatar - 16:lonetrek - 42:metropolis - 43:domain - 32:Sinq Laison
+--  10000065, 10000020, 10000064, 10000068
+
+
+-- categoryID  categoryName
+--  4   Material
+--  5   Accessories
+--  6   Ship
+--  7   Module
+--  8   Charge
+--  9   Blueprint
+--  16  Skill
+--  17  Commodity
+--  18  Drone
+--  20  Implant
+--  22  Deployable
+--  23  Structure
+--  24  Reaction
+--  25  Asteroid
+--  32  Subsystem
+--  34  Ancient Relics
+--  35  Decryptors
+--  39  Infrastructure Upgrades
+--  40  Sovereignty Structures
+--  41  Planetary Interaction
+--  42  Planetary Resources
+--  43  Planetary Commodities
+--  46  Orbitals
