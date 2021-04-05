2670c86e43e7f926eba3de2d046ebd9dfc551e07
Gwendolyn Ryan <gryan@imperiumtechnologies.org>
2021-04-05 15:20:43 -0500

    Changed SQL Scripts charID to ownerID to reflect database changes

--- sql/seed_and_clean/seed_station_market.sql
+++ sql/seed_and_clean/seed_station_market.sql
@@ -14,7 +14,7 @@ insert into tStations values (60014137, 30000053, 10000001);
 -- categoryID IDs and names found in seed_data.sql
 
 -- actual seeding
-INSERT INTO mktOrders (typeID, charID, regionID, stationID, price, volEntered, volRemaining, issued,
+INSERT INTO mktOrders (typeID, ownerID, regionID, stationID, price, volEntered, volRemaining, issued,
 minVolume, duration, solarSystemID, jumps)
   SELECT typeID, stationID, regionID, stationID, basePrice, 550, 550, 132478179209572976, 1, 250, solarSystemID, 1
   FROM tStations, invTypes inner join invGroups on invTypes.groupID=invGroups.groupID
