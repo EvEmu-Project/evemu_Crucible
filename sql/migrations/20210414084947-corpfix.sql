1efe7f8adcaf39c1e25550041593757d6b033b18
James Hirst <jdhirst@hirstgroup.net>
2021-04-14 09:07:56 +0200

    Fixed issue with creating corporation
    Fixed database issue with creating corporation with new db migration.

---
+++ sql/migrations/20210414084947-corpfix.sql
@@ -0,0 +1,8 @@
+
+-- +migrate Up
+ALTER TABLE crpCorporation
+  MODIFY `allianceMemberStartDate` bigint(20) NOT NULL DEFAULT '0';
+
+-- +migrate Down
+ALTER TABLE crpCorporation
+  MODIFY `allianceMemberStartDate` bigint(20) NOT NULL;
