c4dbaa5ab25b4303e87b67e9edaa3f5083d6f1cd
James Hirst <jdhirst@hirstgroup.net>
2021-04-18 18:46:30 +0200

    Cleaned up repo
    Removed tools and other files which are no longer part of the main repo, since they have been moved to the SDK repo. This cleans up the repo and removes non-relevant code and binaries.

--- utils/admin/README.md
+++
@@ -1,2 +0,0 @@
-# Administrator Tools
-This directory contains tools which may be useful to those who administrate EVEmu servers. 
--- utils/playertools/evemon/src/EVEMon.Common/Net/DownloadStringCompletedCallback.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace EVEMon.Common.Net
-{
-    public delegate void DownloadStringCompletedCallback(DownloadStringAsyncResult e, object userState);
-}
