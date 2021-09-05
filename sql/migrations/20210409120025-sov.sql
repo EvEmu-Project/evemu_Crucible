72aa4828eff912f3b25e400a3f7231656a06d17a
James D. Hirst <jdhirst@hirstgroup.net>
2021-09-05 14:37:42 +0200

    Set default onlining time for both IHub and SBU

--- sql/migrations/20210409120025-sov.sql
+++ sql/migrations/20210409120025-sov.sql
@@ -10,6 +10,10 @@ ALTER TABLE mapSystemSovInfo
 MODIFY COLUMN `contested` tinyint(1) NULL DEFAULT 0 AFTER hubID;
 UPDATE dgmTypeAttributes
 SET valueFloat=300000 WHERE attributeID=677 AND typeID=32226;
+UPDATE dgmTypeAttributes
+SET valueFloat=150000 WHERE attributeID=677 AND typeID=32250;
+UPDATE dgmTypeAttributes
+SET valueFloat=300000 WHERE attributeID=677 AND typeID=32458;
 -- +migrate Down
 ALTER TABLE posStructureData
 CHANGE COLUMN `anchorpointID` `moonID` int(10);
@@ -21,3 +25,7 @@ ALTER TABLE mapSystemSovInfo
 MODIFY COLUMN `contested` bit(1) NULL DEFAULT b'0' AFTER hubID;
 UPDATE dgmTypeAttributes
 SET valueFloat=28800000 WHERE attributeID=677 AND typeID=32226;
+UPDATE dgmTypeAttributes
+SET valueFloat=10800000 WHERE attributeID=677 AND typeID=32250;
+UPDATE dgmTypeAttributes
+SET valueFloat=3600000 WHERE attributeID=677 AND typeID=32458;
