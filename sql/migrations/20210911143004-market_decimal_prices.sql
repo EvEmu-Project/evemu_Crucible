13207adb41d3e9fe98b9fd177945a219731fc404
James D. Hirst <jdhirst@hirstgroup.net>
2021-09-12 10:54:01 +0200

    Switched from truncate to drop if exists

--- sql/migrations/20210911143004-market_decimal_prices.sql
+++ sql/migrations/20210911143004-market_decimal_prices.sql
@@ -1,12 +1,13 @@
 -- Replaces float with decimal price storage for market orders. This migration will truncate the table, losing any data which was there before.
 -- +migrate Up
 TRUNCATE mktOrders;
-TRUNCATE seed_migrations;
+DROP TABLE IF EXISTS seed_migrations;
 ALTER TABLE mktOrders
   MODIFY `price` decimal(16,2) NOT NULL DEFAULT '0';
 
 -- +migrate Down
 TRUNCATE mktOrders;
+DROP TABLE IF EXISTS seed_migrations;
 TRUNCATE seed_migrations;
 ALTER TABLE mktOrders
   MODIFY `price` float NOT NULL DEFAULT '0';
