879332806ea097615513cb20348c265abe196c7f
James D. Hirst <jdhirst@hirstgroup.net>
2021-10-15 10:35:46 +0200

    Added default values for beaconID and jammerID to mapSystemSovInfo This commit fixes a segfault on launch caused by beaconID and jammerID missing defaults in the db.

--- sql/migrations/20211014195641-beacon_jammer.sql
+++ sql/migrations/20211014195641-beacon_jammer.sql
@@ -1,9 +1,9 @@
 -- Add columns to sovereignty table for cyno generators and jammers
 -- +migrate Up
 ALTER TABLE mapSystemSovInfo
-ADD COLUMN beaconID INT(10) AFTER hubID;
+ADD COLUMN beaconID INT(10) NOT NULL DEFAULT 0 AFTER hubID;
 ALTER TABLE mapSystemSovInfo
-ADD COLUMN jammerID INT(10) AFTER beaconID;
+ADD COLUMN jammerID INT(10) NOT NULL DEFAULT 0 AFTER beaconID;
 
 -- +migrate Down
 ALTER TABLE mapSystemSovInfo
